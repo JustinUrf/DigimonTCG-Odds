4bb96e700618552f605d0049c1ba643022669dc7
Justin Lee <lee.justin001126@gmail.com>
2023-05-06 10:47:39 -0700

    resolved webpack issues, working on functionality now

--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,25 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
-
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devServer: {
-    contentBase: './dist'
-  },
   devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
   plugins: [
-    new CleanWebpackPlugin({
-      verbose: true
-    }),
+    new Dotenv(),
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Capstone Project',
       template: './src/index.html',
       inject: 'body'
-    }),
-    new Dotenv(),
+    })
   ],
   module: {
     rules: [
