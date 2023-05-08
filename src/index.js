496f0a3421c624462dc63d69fa5069348e5778b8
Justin Lee <lee.justin001126@gmail.com>
2023-05-07 19:11:41 -0700

    add test plot with mock data when pressing submit button, working on grabbing all the levels to display deck now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'plotly.js-dist-min';
+import 'plotly.js';
+
+let Plotly = require('plotly.js')
 
 function handleDeckForm(event) {
   event.preventDefault();
-  document.querySelector('#response').innerText = null;
+  document.querySelector('#rookieOdds').innerText = "Hi there";
+  let TESTER = document.getElementById('tester');
+	Plotly.newPlot( TESTER, [{
+	x: [1, 2, 3, 4, 5],
+	y: [1, 2, 4, 8, 16] }], {
+	margin: { t: 0 } } ); 
 }
 
 window.addEventListener("load", function() {
